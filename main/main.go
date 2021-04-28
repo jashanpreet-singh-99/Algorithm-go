5eb4d93174a930f364a32cd0447312d5b82b116e
unknown <jashan.singh.work@icloud.com>
2021-04-28 20:20:41 +0530

    Added matrix Scaler Addition.

--- main/main.go
+++ main/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+  "fmt"
+  "reflect"
   "jashanpreet_singh_99/matrix"
   )
 
 func main() {
   mat := matrix.RandomMatrix(3,3)
-  matrix.Mprint(mat)
+  fmt.Println(reflect.TypeOf(mat))
+  mat.Print()
+  mat.Add(10)
+  mat.Print()
+  mat_2 := matrix.RandomMatrix(3,3)
+  mat_2.print()
+  mat.Add(mat_2)
+  mat.print()
 }
--- matrix/matrix.go
+++ matrix/matrix.go
@@ -6,6 +6,15 @@ import (
   "fmt"
 )
 
+type Scalar_Vector interface {
+  Add()
+}
+
+type Mat struct{
+  Value [][]int
+  Shape []int
+}
+
 var seeded bool // Which if seed is initialized
 var rand_1 *rand.Rand // the rand object linked to seed
 
@@ -14,9 +23,9 @@ Objective := generate a random matrix of given size
 parameters :-
           r : number of rows
           c : number of columns
-return := [][]int
+return := Mat Objective
 */
-func RandomMatrix(r int, c int) [][]int {
+func RandomMatrix(r int, c int) Mat {
   if seeded == false {
     x1 := rand.NewSource(time.Now().UnixNano())
     rand_1 = rand.New(x1)
@@ -30,19 +39,54 @@ func RandomMatrix(r int, c int) [][]int {
     }
     temp = append(temp, temp_r)
   }
-  return temp
+  return Mat{temp, []int{r,c}}
 }
 
 /*
 Objective := print the matrix in readable format
-parameters :-
-          mat : [][]int the mat to print
 */
-func Mprint(mat [][]int) {
-  r := len(mat)
-  c := len(mat[0])
+func (mat Mat) Print() {
+  r := mat.Shape[0]
+  c := mat.Shape[1]
   for i := 0; i < r; i++ {
-    fmt.Println(mat[i])
+    fmt.Printf("[ ")
+    for j := 0; j < c; j++ {
+      fmt.Printf("%02d ", mat.Value[i][j])
+    }
+    fmt.Printf("]\n")
   }
   fmt.Printf("Shape (%d,%d)\n", r,c)
+}
+
+/*
+Objective := add scaler value to matrix
+*/
+func (mat Mat) Add(s int) {
+  for i := 0 ; i < mat.Shape[0]; i++ {
+    for j := 0 ; j < mat.Shape[1]; j++ {
+      mat.Value[i][j] = mat.Value[i][j] + s
+    }
+  }
+}
+
+/*
+Objective := add scaler value to matrix
+*/
+func (mat Mat) Add(mat_2 Mat) {
+  if len(mat.Shape) != len(mat_2.Shape) {
+    fmt.Println("Miss-matched dimmensions.")
+    return
+  } else {
+    for dim := 0 ; dim < len(mat.Shape); dim++ {
+      if mat.Shape[dim] != mat.Shape[dim] {
+        fmt.Println("Miss-matched shape at : ", dim)
+        return
+      }
+    }
+  }
+  for i := 0 ; i < mat.Shape[0]; i++ {
+    for j := 0 ; j < mat.Shape[1]; j++ {
+      mat.Value[i][j] = mat.Value[i][j] + mat_2.Value[i][j]
+    }
+  }
 }
